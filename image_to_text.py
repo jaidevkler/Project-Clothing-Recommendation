0c56a796ea4ade3b0819e73a988295c4d345995e
jaidevkler <jaidev@kler.net>
2024-08-25 13:26:10 -0400

    Added comments

--- image_to_text.py
+++ image_to_text.py
@@ -8,12 +8,15 @@ def encode_image(image_path):
     with open(image_path, "rb") as image_file:
       return base64.b64encode(image_file.read()).decode('utf-8')
 
-
+# Function to convert image to text
 def image_to_text(image_path, category):
+    # Upper category
     if category == 'upper':
         text = f"{category.capitalize()} clothing gender, color, type, length, fit, style, details, accents, neckline, sleeve, pattern. Message should only include these details. Don't inlude none categories."
+    # Lower category
     elif category == 'lower':
        text = f"{category.capitalize()} clothing gender, color, type, length, fit, style, material/fabric, pattern, accents. Message should only include these details. Don't inlude none categories."
+    # Shoes category
     else:
        text = f"{category.capitalize()} gender, color, brand, collection, type, style, material, toe shape, heel type, sole type, fastening, pattern, accents, purpose. Message should only include these details"
     # Load OpenAI api key
